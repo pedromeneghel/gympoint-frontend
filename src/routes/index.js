2d0c3a473f1a9a001df4d51ddc064c378eecf060
Pedro Meneghel <pedromeneghel@gmail.com>
2019-12-20 07:01:00 -0300

    Preparação do ambiente

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Routes';
+
+import SignIn from '../pages/SignIn';
+
+/* import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile'; */
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+
+      <Route path="/" component={() => <h1>404</h1>} />
+    </Switch>
+  );
+}
